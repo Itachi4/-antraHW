2297d8188506eb8368810b3edcf16d55cca9194d
Itachi4 <60189482+Itachi4@users.noreply.github.com>
2024-07-24 11:05:24 -0400

    Delete nottflix_final directory

--- nottflix_final/front-end/src/app/pages/home/components/home-row-three/home-row-three.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home-row-three',
-  templateUrl: './home-row-three.component.html',
-  styleUrl: './home-row-three.component.scss',
-})
-export class HomeRowThreeComponent {}
